4e2b51dd53bb7fb8aae43c591c96083258c63ef6
wjyang3826 <40921915+wjyang3826@users.noreply.github.com>
2018-11-13 13:55:05 +0800

    Add files via upload
    Module_B First Update

---
+++ src/customer/maintenance/and/invoice/payment/CustomerMaintenanceAndInvoicePayment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customer.maintenance.and.invoice.payment;
+
+/**
+ *
+ * @author Atlas
+ */
+public class CustomerMaintenanceAndInvoicePayment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
